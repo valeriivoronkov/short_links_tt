413981d6b9cd470b129e0ec02485a500e223fed8
xcoder <onlyforwith@gmail.com>
2026-07-04 19:56:04 +0300

    Update LinkVisit

--- app/Models/LinkVisit.php
+++ app/Models/LinkVisit.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $short_link_id
+ * @property string $ip_address
+ */
 class LinkVisit extends Model
 {
-    //
+    protected $fillable = [
+        'short_link_id',
+        'ip_address',
+    ];
+
+    public function shortLink(): BelongsTo
+    {
+        return $this->belongsTo(ShortLink::class);
+    }
 }
