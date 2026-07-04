73a99321fb3cb5076787f2931d58023caea44be4
xcoder <onlyforwith@gmail.com>
2026-07-04 16:25:42 +0300

    Fix redirect from register to /admin

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
+//Route::middleware(['auth'])->group(function () {
+//    Route::view('dashboard', 'dashboard')->name('dashboard');
+//});
 
 require __DIR__.'/settings.php';
