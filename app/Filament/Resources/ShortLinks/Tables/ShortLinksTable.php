0fb33b8d4d0e1985b250a528fb21d2f9e054188c
xcoder <onlyforwith@gmail.com>
2026-07-04 17:48:28 +0300

    Update ShortLinksTable

--- app/Filament/Resources/ShortLinks/Tables/ShortLinksTable.php
+++ app/Filament/Resources/ShortLinks/Tables/ShortLinksTable.php
@@ -4,7 +4,8 @@ namespace App\Filament\Resources\ShortLinks\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class ShortLinksTable
@@ -13,13 +14,29 @@ class ShortLinksTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('original_url')
+                    ->label('Original URL')
+                    ->searchable()
+                    ->limit(50),
+
+                TextColumn::make('short_code')
+                    ->label('Short Code')
+                    ->copyable(),
+
+                TextColumn::make('clicks_count')
+                    ->label('Clicks')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->dateTime('d.m.Y H:i')
+                    ->sortable(),
             ])
             ->filters([
                 //
             ])
             ->recordActions([
-                EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
