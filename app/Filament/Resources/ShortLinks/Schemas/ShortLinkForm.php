c6506ab4bfea74ff512b85a77e309aea1ffbb84a
xcoder <onlyforwith@gmail.com>
2026-07-04 17:30:55 +0300

    Update ShortLinkForm

--- app/Filament/Resources/ShortLinks/Schemas/ShortLinkForm.php
+++ app/Filament/Resources/ShortLinks/Schemas/ShortLinkForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\ShortLinks\Schemas;
 
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class ShortLinkForm
@@ -10,7 +11,11 @@ class ShortLinkForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('original_url')
+                    ->label('Original URL')
+                    ->url()
+                    ->required()
+                    ->maxLength(2048),
             ]);
     }
 }
