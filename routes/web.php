f80812bcd841e360c3e56f170ef5a452e189cbbd
xcoder <onlyforwith@gmail.com>
2026-07-05 11:33:46 +0300

    Update web.php with shortCode route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RedirectController;
 
 Route::view('/', 'welcome')->name('home');
+
+Route::get('/{shortCode}', RedirectController::class)
+    ->name('short-links.redirect');
 
 require __DIR__.'/settings.php';
